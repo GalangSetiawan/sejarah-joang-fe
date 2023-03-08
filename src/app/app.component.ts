5fe1e4ea788ad62da3d4184409d30916a7a8a720
Galang Setiawan <52562711+GalangSetiawan@users.noreply.github.com>
2023-03-08 15:22:52 +0000

    ssdsds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public sidebarShow: boolean = false;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private route: Router,
+
+    ) {
+  }
+
+  ngOnInit() {
+    
+  }
+
+
+  public setCurrentMenu(url:string){
+    console.log('setCurrentMenu ===>', this.route.url);
+    if(this.route.url.includes(url)) return true;
+    else return false;
+  }
 }
