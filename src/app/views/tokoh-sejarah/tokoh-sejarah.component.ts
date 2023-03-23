97e3d5ebb449e54d927a27379489218c023b124d
Galang Setiawan <52562711+GalangSetiawan@users.noreply.github.com>
2023-03-23 23:03:05 +0000

    sdsds

--- src/app/views/tokoh-sejarah/tokoh-sejarah.component.ts
+++ src/app/views/tokoh-sejarah/tokoh-sejarah.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class TokohSejarahComponent {
 
+  public listTokoh = [];
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.listTokoh = [
+      { name : "SUKARNI KARTODIWIRJO" },
+      { name : "CHAIRUL SALEH" },
+      { name : "DIPA NUSANTARA AIDIT" },
+      { name : "ADAM MALIK" },
+      { name : "WIKANA" },
+      { name : "MUHAMMAD HATTA LUKMAN" },
+      { name : "NJONO MARUTO NITIMIHARDJO" },
+      { name : "ANAK MARHAEN HANAFI" },
+      { name : "BURHANUDDIN MOHAMMAD DIAH" },
+      { name : "AMIR SJARIFOEDDIN" },
+      { name : "SUBADIO SASTROSATOMO" },
+      { name : "DJOHAR NUR" },
+    ]
+  }
 }
